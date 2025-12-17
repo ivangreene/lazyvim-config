90df95b0653de2a8708e128cf5133ca293a56b4a
Ivan Greene <ivan@ivan.sh>
2025-12-17 11:46:00 -0500

    Add neotest-vitest adapter

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,11 @@
+return {
+  "https://github.com/nvim-neotest/neotest.git",
+  dependencies = {
+    "https://github.com/marilari88/neotest-vitest",
+  },
+  opts = {
+    adapters = {
+      ["neotest-vitest"] = {},
+    },
+  },
+}
