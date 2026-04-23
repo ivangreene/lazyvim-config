22e9ff5abdc209281800a1e6476e8f531b0dc2d7
Ivan Greene <ivan@ivan.sh>
2026-04-23 14:33:29 -0400

    Configure mise

---
+++ lua/plugins/ruby-lsp.lua
@@ -0,0 +1,17 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruby_lsp = {
+        cmd = { "mise", "exec", "--", "ruby-lsp" },
+        init_options = {
+          addonSettings = {
+            ["Ruby LSP Rails"] = {
+              enablePendingMigrationsPrompt = false,
+            },
+          },
+        },
+      },
+    },
+  },
+}
